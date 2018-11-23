16a3842eb9c2e51606aa67003c395b5d132c4d9c
CedricO-IMIE <cedric.olivry@imie.fr>
2018-11-23 11:29:01 +0100

    idea

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
 
+$errors = array();
+
+if(empty($_GET["email"])) {
+    $errors[] = "Veuillez rentrer une adresse mail valide";
+}
+
+if(empty($_GET["mdp"])) {
+    $errors[] = "Veuillez rentrer un mot de passe valide";
+}
 
 
-?>
-<html>
-    <head>
-        <title>Login</title>
-        <link rel="stylesheet" type="text/css" href="index.css">
-    </head>
-    <body>
-        <h1>Connexion</h1>
-    </body>
-</html>
+?><!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Login</title>
+            <link rel="stylesheet" type="text/css" href="index.css">
+        </head>
+        <body>
+        <?php if(!empty($errors)): ?>
+        <?php foreach ($errors as $msg): ?>
+            <p><?php echo $msg; ?></p>
+        <?php endforeach; ?>
+        <?php endif; ?>
+        <br><br><h1>Connexion</h1><br><br><br><br><br><br>
+            <input id="email" name="email" type="email" placeholder="Adresse mail" class="type"/><br><br><br><br>
+            <input id="mdp" name="mdp" type="password" placeholder="Mot de Passe" class="type"/><br><br><br>
+            <button type="submit" class="button">Valider</button>
+        </body>
+    </html>
