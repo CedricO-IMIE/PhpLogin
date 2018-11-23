df800a593847d487ee8ceb0155602b81d280b21f
CedricO-IMIE <cedric.olivry@imie.fr>
2018-11-23 10:04:19 +0100

    page login

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+?>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+        <h1>Connexion</h1>
+    </body>
+</html>
